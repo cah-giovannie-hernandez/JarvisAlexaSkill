75d06960eef6c891d9a1596ab9854b56de3ba27c
jazzy1331 <jassu.junk@gmail.com>
2016-04-23 14:57:07 -0400

    Built the Get hero module

--- jarvis.js
+++ jarvis.js
@@ -2,6 +2,69 @@ exports.handler = function( event, context ) {
 };
 
 function getSuperhero(intent, context) {
+
+	var heroId;
+	var description;
+	var heroName;
+	var heroImage;
+
+	var PRIV_KEY = "68a9ac0460981d000acb70fba3a1db88e2408fdb";
+	var PUB_KEY = "6864eb209818465f2af97ba120632a3b";
+	//var ts = new Date.().GetTime();
+	var hash = crypto.createHash('md5').update("20160423" + PRIV_KEY + PUB_KEY).digest('hex');
+
+	var url = "http://gateway.marvel.com:80/v1/public/characters?name=" + heroName + "&apikey=" +
+			  PUB_KEY + "&ts=20160423"; 
+
+	var http = require( 'http' );
+
+    http.get( url, function( response ) {
+        
+        var data = '';
+        
+        response.on( 'data', function( x ) { data += x; } );
+        
+        response.on( 'end', function() {
+            
+            var json = JSON.parse(data);
+
+
+            if(json.data.results.length > 0){
+
+            heroId = json.data.results[0].id;
+            description = json.data.results[0].description;
+            name = json.data.results[0].name;
+            image = json.data.results[0].thumbnail.path + "." + json.data.results[0].thumbnail.extension;
+        }
+    } );
+        
+    } );
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+	//console.log(hash);
+
+	// var superHeroDescriptors [
+	// 	{var heroId;}
+	// 	{var description;}
+	// 	{var heroName;}
+	// 	{var heroImage;}
+	// ];
+
+
+
+
 }
 
 function sendOutput(cardTitle, text, repromptText, shouldEndSession, context) {
