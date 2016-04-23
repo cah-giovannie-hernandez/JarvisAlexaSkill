8089b258b0c52792ee10efdc65f3cbdd1bf91536
jazzy1331 <jassu.junk@gmail.com>
2016-04-23 15:00:35 -0400

    Saaved all the changes now :)

--- jarvis.js
+++ jarvis.js
@@ -7,10 +7,10 @@ function getSuperhero(intent, context) {
 	var description;
 	var heroName;
 	var heroImage;
+	var speechOutput;
 
 	var PRIV_KEY = "68a9ac0460981d000acb70fba3a1db88e2408fdb";
 	var PUB_KEY = "6864eb209818465f2af97ba120632a3b";
-	//var ts = new Date.().GetTime();
 	var hash = crypto.createHash('md5').update("20160423" + PRIV_KEY + PUB_KEY).digest('hex');
 
 	var url = "http://gateway.marvel.com:80/v1/public/characters?name=" + heroName + "&apikey=" +
@@ -28,43 +28,28 @@ function getSuperhero(intent, context) {
             
             var json = JSON.parse(data);
 
-
             if(json.data.results.length > 0){
 
-            heroId = json.data.results[0].id;
-            description = json.data.results[0].description;
-            name = json.data.results[0].name;
-            image = json.data.results[0].thumbnail.path + "." + json.data.results[0].thumbnail.extension;
-        }
-    } );
+	            heroId = json.data.results[0].id;
+	            description = json.data.results[0].description;
+	            heroName = json.data.results[0].name;
+	            heroImage = json.data.results[0].thumbnail.path + "." + json.data.results[0].thumbnail.extension;
+      	  		if(description === ""){
+      	  			speechOutput = heroName + " does not have a description"
+      	  		}else{
+      	  			speechOutput = heroName + ", " + description;
+      	  		}
+      	  }else{
+
+      	  	var text = "Your requested superhero does not exist";
+      	  	var cardTitle = "Invalid Superhero";
+      	  	var shouldEndSession = true;
+      	  	sendOutput(cardTitle, text, "", shouldEndSession, context);
+
+        	}
+    	} );
         
-    } );
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-	//console.log(hash);
-
-	// var superHeroDescriptors [
-	// 	{var heroId;}
-	// 	{var description;}
-	// 	{var heroName;}
-	// 	{var heroImage;}
-	// ];
-
-
-
-
+    });
 }
 
 function sendOutput(cardTitle, text, repromptText, shouldEndSession, context) {
